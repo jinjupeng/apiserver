4eb9b2818452f6bdad0e6f887bc21256f8dcde55
jinjupeng <im.jp@outlook.com>
2019-06-21 19:11:18 +0800

    配置日志文件

--- config/config.go
+++ config/config.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
+
+	"github.com/lexkong/log"
 )
 
 type Config struct {
@@ -21,7 +23,8 @@ func Init(cfg string) error {
 	if err := c.initConfig(); err != nil {
 		return err
 	}
-
+	// 初始化日志包
+	c.initLog()
 	// 监控配置文件变化并热加载程序
 	c.watchConfig()
 
@@ -46,11 +49,32 @@ func (c *Config) initConfig() error {
 
 	return nil
 }
+func (c *Config) initLog() {
+    passLagerCfg := log.PassLagerCfg {
+		// 输出位置：file/stdout。选择file将日志记录到logger_file 指定的日志文件中，选择 stdout 会将日志输出到标准输出，当然也可以两者同时选择
+		Writers:        viper.GetString("log.writers"),
+		// 日志级别：DEBUG、INFO、WARN、ERROR、FATAL
+		LoggerLevel:    viper.GetString("log.logger_level"),
+		// 日志文件
+		LoggerFile:     viper.GetString("log.logger_file"),
+		// 日志的输出格式，JSON 或者 plaintext，true 会输出成非 JSON 格式，false 会输出成 JSON 格式
+		LogFormatText:  viper.GetBool("log.log_format_text"),
+		// otate 依据，可选的有 daily 和 size。如果选 daily 则根据天进行转存，如果是 size 则根据大小进行转存
+		RollingPolicy:  viper.GetString("log.rollingPolicy"),
+		// rotate 转存时间，配 合rollingPolicy: daily 使用
+		LogRotateDate:  viper.GetInt("log.log_rotate_date"),
+		// rotate 转存大小，配合 rollingPolicy: size 使用
+		LogRotateSize:  viper.GetInt("log.log_rotate_size"),
+		// 当日志文件达到转存标准时，log 系统会将该日志文件进行压缩备份，这里指定了备份文件的最大个数
+        LogBackupCount: viper.GetInt("log.log_backup_count"),
+    }
 
+    log.InitWithConfig(&passLagerCfg)
+}  
 // 监控配置文件变化并热加载程序
 func (c *Config) watchConfig() {
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
-		log.Printf("Config file changed: %s", e.Name)
+		log.Infof("Config file changed: %s", e.Name)
 	})
 }
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"github.com/lexkong/log"
 )
 
 var (
@@ -47,11 +48,11 @@ func main() {
 		if err := pingServer(); err != nil {
 			log.Fatal("The router has no response, or it might took too long to start up.", err)
 		}
-		log.Print("The router has been deployed successfully.")
+		log.Info("The router has been deployed successfully.")
 	}()
 
-	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
-	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
+	log.Infof("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
+	log.Info(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
 
 // pingServer pings the http server to make sure the router is working.
@@ -65,7 +66,7 @@ func pingServer() error {
 		}
 
 		// Sleep for a second to continue the next ping.
-		log.Print("Waiting for the router, retry in 1 second.")
+		log.Info("Waiting for the router, retry in 1 second.")
 		time.Sleep(time.Second)
 	}
 	return errors.New("Cannot connect to the router.")

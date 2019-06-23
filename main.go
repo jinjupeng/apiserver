771d7fa20229410ff9171c8bfebc92183647b1ce
jinjupeng <im.jp@outlook.com>
2019-06-23 14:46:00 +0800

    给API命令增加版本功能

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"apiserver/config"
 	"apiserver/model"
+	v "apiserver/pkg/version"
 	"apiserver/router"
 	"apiserver/router/middleware"
 
@@ -17,11 +21,23 @@ import (
 )
 
 var (
-	cfg = pflag.StringP("config", "c", "", "apiserver config file path.")
+	cfg     = pflag.StringP("config", "c", "", "apiserver config file path.")
+	version = pflag.BoolP("version", "v", false, "show version info.")
 )
 
 func main() {
 	pflag.Parse()
+	if *version {
+		v := v.Get()
+		marshalled, err := json.MarshalIndent(&v, "", "  ")
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(marshalled))
+		return
+	}
 
 	// 初始化配置
 	if err := config.Init(*cfg); err != nil {
